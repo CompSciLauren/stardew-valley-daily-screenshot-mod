c4f28c8e67244838da31ad32a46c10d0f605a08d
Lauren Stephenson <compscilauren@gmail.com>
2019-12-03 16:52:28 -0600

    Add everything except actually taking the screenshot

--- StardewValleyDailyScreenshotMod/ModEntry.cs
+++ StardewValleyDailyScreenshotMod/ModEntry.cs
@@ -10,6 +10,8 @@ namespace StardewValleyDailyScreenshotMod
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
+        bool screenshotTakenToday = false;
+
         /*********
         ** Public methods
         *********/
@@ -17,9 +19,9 @@ namespace StardewValleyDailyScreenshotMod
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.Player.Warped += this.OnNewLocationEntered;
+            helper.Events.GameLoop.DayEnding += this.SetScreenshotTakenTodayToFalse;
         }
-
 
         /*********
         ** Private methods
@@ -27,14 +29,51 @@ namespace StardewValleyDailyScreenshotMod
         /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        private void OnNewLocationEntered(object sender, WarpedEventArgs e)
         {
             // ignore if player hasn't loaded a save yet
             if (!Context.IsWorldReady)
                 return;
 
             // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+            this.Monitor.Log($"{Game1.player.Name} exited {e.OldLocation} and entered {e.NewLocation}", LogLevel.Debug);
+
+            if (e.NewLocation is StardewValley.Farm && !screenshotTakenToday)
+            {
+                TakeScreenshot();
+            }
+            else
+            {
+                this.Monitor.Log($"Either location is not Farm or screenshot was already taken today.", LogLevel.Debug);
+            }
+        }
+
+        /// <summary>Takes a screenshot of the farm from a bird's eye view.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void TakeScreenshot()
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print screenshot taken to the console window
+            this.Monitor.Log($"Setting screenshotTakenToday to true.", LogLevel.Debug);
+            screenshotTakenToday = true;
+        }
+
+        /// <summary>Takes a screenshot of the farm from a bird's eye view.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void SetScreenshotTakenTodayToFalse(object sender, DayEndingEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print screenshot taken to the console window
+            this.Monitor.Log($"Setting screenshotTakenToday to false.", LogLevel.Debug);
+            screenshotTakenToday = false;
         }
     }
 }
