5ceae5d8e3282ee962f02eb0d0dcaac4c37afa49
Lauren Stephenson <compscilauren@gmail.com>
2019-12-04 10:47:08 -0600

    Add multiple screenshot folders

--- DailyScreenshot/ModEntry.cs
+++ DailyScreenshot/ModEntry.cs
@@ -24,11 +24,20 @@ namespace DailyScreenshot
             {
                 exportDirectory = stardewValleyRootDirectory.CreateSubdirectory("MapExport");
             }
-
             CreateFileSystemWatcher();
-            screenshotsDirectory = exportDirectory.CreateSubdirectory("Screenshots");
-            helper.Events.Player.Warped += OnNewLocationEntered;
-            helper.Events.GameLoop.DayEnding += SetScreenshotTakenTodayToFalse;
+            helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+
+        /// <summary>Raised after the save file is loaded.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            var farmName = Game1.player.farmName;
+            var directoryName = $"{farmName}-Farm-Screenshots";
+            screenshotsDirectory = exportDirectory.CreateSubdirectory(directoryName);
+            Helper.Events.Player.Warped += OnNewLocationEntered;
+            Helper.Events.GameLoop.DayEnding += SetScreenshotTakenTodayToFalse;
         }
 
         /// <summary>Creates a new FileSystemWatcher and set its properties.</summary>
