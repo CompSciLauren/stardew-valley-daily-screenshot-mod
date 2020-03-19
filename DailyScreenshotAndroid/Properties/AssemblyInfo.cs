fed9de5312cda589d01f3aef09f58c2f5212460c
Lauren Stephenson <compscilauren@gmail.com>
2020-03-18 22:23:00 -0500

    Rename dll

--- DailyScreenshotAndroid/Properties/AssemblyInfo.cs
+++ DailyScreenshotAndroid/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StardewValleyModTemplate")]
+[assembly: AssemblyTitle("DailyScreenshotAndroid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StardewValleyModTemplate")]
+[assembly: AssemblyProduct("DailyScreenshotAndroid")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
